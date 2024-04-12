dd0a4321cda7d36c19b8dd3378be0adce74060de
Melanie <melanie.collins.p@gmail.com>
2024-04-12 13:54:16 -0400

    rutas dinámicas de services

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { engine } from 'express-handlebars';
 
+import { services } from './data/services.data.js'
+
 const app = express();
 
 // public directory
@@ -16,11 +18,21 @@ app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { title: "Home Page 2.0" });
 });
 
 app.get('/services', (req, res) => {
-    res.render('services');
+    res.render('services', { services: services });
+});
+
+app.get('/services/:name', (req, res) => {
+
+    const nameURL = req.params.name
+
+    const service = services.find((item) => item.url === `/services/${nameURL}`)
+    console.log(service)
+
+    res.render('service', { service })
 });
 
 app.listen(5000, () => console.log(`Servidor encendido http://localhost:${5000}`))
