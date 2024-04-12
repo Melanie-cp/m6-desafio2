93ae3b1822eeab2bb731de3721f133884c979333
Melanie <melanie.collins.p@gmail.com>
2024-04-12 15:55:19 -0400

    rutas dinámicas y 404

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import express from 'express'
 import { engine } from 'express-handlebars';
 
 import { services } from './data/services.data.js'
+import path from 'path'
 
 const app = express();
 
-// public directory
-app.use(express.static('public'))
+const __dirname = import.meta.dirname;
 
-app.use('/css', express.static('node_modules/bootstrap/dist/css'))
-app.use('/js', express.static('node_modules/bootstrap/dist/js'))
-app.use('/css', express.static('node_modules/jquery/dist'))
+// public directory
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
 
 // handlebars
 app.engine('.hbs', engine({ extname: '.hbs' }));
@@ -18,11 +21,11 @@ app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home', { title: "Home Page 2.0" });
+    return res.render('home', { title: "Home Page 2.0" });
 });
 
 app.get('/services', (req, res) => {
-    res.render('services', { services: services });
+    return res.render('services', { services: services });
 });
 
 app.get('/services/:name', (req, res) => {
@@ -30,9 +33,18 @@ app.get('/services/:name', (req, res) => {
     const nameURL = req.params.name
 
     const service = services.find((item) => item.url === `/services/${nameURL}`)
-    console.log(service)
 
-    res.render('service', { service })
+    //undefined 
+    if (!service) {
+        return res.render('404', { title: "No se encuentra el servicio" })
+    }
+
+    return res.render('service', { service })
 });
+
+//404 para cualquier otra ruta
+app.get('/*', (req, res) => {
+    return res.status(404).render('404', { title: "No se encuentra la página 😣" })
+})
 
 app.listen(5000, () => console.log(`Servidor encendido http://localhost:${5000}`))
