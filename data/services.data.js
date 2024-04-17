244b550b54ace644f8f7303cefc6005592a6a4fd
Melanie <melanie.collins.p@gmail.com>
2024-04-16 23:05:40 -0400

    service page

--- data/services.data.js
+++ data/services.data.js
@@ -3,40 +3,48 @@ export const services = [
         name: "Web Development",
         description: "We create web applications with the latest technologies.",
         url: "/services/web-development",
+        img: "https://timesproweb-static-backend-prod.s3.ap-south-1.amazonaws.com/Web_Development_Banner_Image_0e476d1ab6.webp"
     },
     {
         name: "Mobile Development",
         description: "We create mobile applications with the latest technologies.",
         url: "/services/mobile-development",
+        img: "https://img.freepik.com/vector-gratis/fondo-degradado-ui-ux_23-2149065783.jpg?size=626&ext=jpg&ga=GA1.1.1488620777.1708128000&semt=ais"
     },
     {
         name: "DevOps",
         description: "We create CI/CD pipelines with the latest technologies.",
         url: "/services/devops",
+        img: "https://www.qiminfo.ch/wp-content/uploads/2023/05/The-DevOps-Lifecycle-1024x576.jpg"
     },
     {
         name: "QA",
         description: "We create test cases with the latest technologies.",
         url: "/services/qa",
+        img: "https://inlogiq.com/wp-content/uploads/2021/06/blog-QA-Testing.jpg"
     },
     {
         name: "UX/UI",
         description: "We create designs with the latest technologies.",
         url: "/services/ux-ui",
+        img: "https://excellencesol.com/wp-content/uploads/2023/08/ux-ui.png"
     },
     {
         name: "Training",
         description: "We create training with the latest technologies.",
         url: "/services/training",
+        img: "https://assets-global.website-files.com/6254ae25c3e3a2cdc526ebaf/63644a84ec09440a05e1b7ec_image.jpeg"
     },
     {
         name: "Consulting",
         description: "We create consulting with the latest technologies.",
         url: "/services/consulting",
+        img: "https://gdcitsolutions.com/wp-content/uploads/Consulting-Overview.jpg"
     },
     {
         name: "Outsourcing",
         description: "We create outsourcing with the latest technologies.",
         url: "/services/outsourcing",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGxJakESVhvKdJ4hCbFJADhXPy4ZV2SmeCwUw9Za1H0w&s"
     },
 ];
